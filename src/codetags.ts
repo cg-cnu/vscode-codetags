b9e41ea39f73e245609d2f52a3a601ab8ed1010c
cg-cnu <sreenivas9alapati@gmail.com>
2018-04-23 17:35:56 +0530

    rewrote to read custom tags from settings

--- src/codetags.ts
+++ src/codetags.ts
@@ -1,47 +1,70 @@
-'use strict';
-import * as vscode from 'vscode';
-import * as tags from './tags'
+"use strict";
+import * as vscode from "vscode";
 
-// get tag names
+const getDate: any = () => new Date(Date.now()).toLocaleString();
+const getUser: any = () => require("os").userInfo().username;
+
+// Default tags
+var tags: any = {
+  todo: "Tasks pending completion",
+  fixme: "Problematic code needing refactor or cleanup",
+  bug: "Reported defects",
+  idea: "Possible RFE candidates",
+  wtf: "Misunderstood details",
+  hack: "Temporary fix",
+  note: "Pointers to other sources"
+};
+
+// get custom tags
+const customTags: Array<any> = vscode.workspace.getConfiguration("codetags").custom;
+console.log(customTags);
+for (let tag of customTags) {
+  tags[tag.name] = tag.body;
+}
+
 var tagNames: Array<string> = [];
-for (let tag of Object.keys(tags)){
-    tagNames.push(tag)
+for (let tag of Object.keys(tags)) {
+  tagNames.push(tag);
 }
 
 // insert the tags
-var insertTag: any = (tag: string) => {
-    const editor: vscode.TextEditor = vscode.window.activeTextEditor;
-    // if no active text editor
-    if (!editor) {
-        vscode.window.showErrorMessage("No file open.");
-        return;
-    }
-    editor.edit( editBuilder => {
-        // delete the selected text
-        editBuilder.delete(editor.selection);
-        // insert the tag text
-        editBuilder.insert(editor.selection.start, tags[tag]());
-    }).then( () =>{
-        // comment the inserted lines
-        vscode.commands.executeCommand("editor.action.commentLine") 
+var insertTag: any = (editor: vscode.TextEditor, tag: string) => {
+  editor
+    .edit(editBuilder => {
+      // delete the selected text
+      editBuilder.delete(editor.selection);
+      // insert the tag text
+      editBuilder.insert(
+        editor.selection.start,
+        `${tag.toUpperCase()}: ${tags[tag]} -@${getUser()} at ${getDate()}`
+      );
     })
+    .then(() => {
+      // comment the inserted lines
+      vscode.commands.executeCommand("editor.action.commentLine");
+      vscode.commands.executeCommand("editor.action.insertLineAfter");
+      vscode.commands.executeCommand("editor.action.commentLine");
+    });
 };
 
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("codetags.tags", () => {
+      const editor: vscode.TextEditor = vscode.window.activeTextEditor;
+      // if no active text editor
+      if (!editor) {
+        vscode.window.showErrorMessage("No file open.");
+        return;
+      }
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('codetags.tags', () => { 
-            // provide code tags 
-            vscode.window.showQuickPick( tagNames )
-            .then( (tag: string) => {
-                // insert tag
-                if(tag){
-                    insertTag(tag);
-                }
-            });
-        })            
-    );    
+      vscode.window.showQuickPick(tagNames).then((tag: string) => {
+        // insert tag
+        if (tag) {
+          insertTag(editor, tag);
+        }
+      });
+    })
+  );
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
