fb3beb524e5022984b0bd584f09dd75ef71871b3
cg-cnu <sreenivas9alapati@gmail.com>
2020-04-24 14:50:22 +0930

    major refactor

--- src/codetags.ts
+++ src/codetags.ts
@@ -1,32 +1,60 @@
 "use strict";
 import * as vscode from "vscode";
+import { format } from "path";
 
-const getDate: any = () => new Date(Date.now()).toLocaleString();
-const getUser: any = () => require("os").userInfo().username;
+const getDate = (format = "default"): string => {
+  // determine the format
+  return new Date(Date.now()).toLocaleString();
+};
+const getUser = (): string => require("os").userInfo().username;
 
 // Default tags
-var tags: any = {
-  todo: "Tasks pending completion",
-  fixme: "Needing refactor or cleanup",
-  bug: "Reported defects",
-  idea: "Possible implementations",
-  wtf: "Misunderstood details",
-  hack: "Temporary fix",
-  note: "Needs discussion or investigation",
-};
+var defaultTags: Array<any> = [
+  {
+    name: "todo",
+    description: "Tasks pending completion",
+  },
+  {
+    name: "fixme",
+    description: "Needing refactor or cleanup",
+  },
+];
 
-// get custom tags
-const customTags: Array<any> = vscode.workspace.getConfiguration("codetags")
-  .custom;
-for (let tag of customTags) {
-  tags[tag.name] = tag.body;
-}
+// todo: "Tasks pending completion",
+// fixme: "Needing refactor or cleanup",
+// bug: "Reported defects",
+// idea: "Possible implementations",
+// wtf: "Misunderstood details",
+// hack: "Temporary fix",
+// note: "Needs discussion or investigation",
+
+const config: any = vscode.workspace.getConfiguration("codetags");
+// add custom tags on top of default tags
+const tags = [...defaultTags, ...config.custom];
+// customTags.forEach((tag) => {
+//   tags[tag.name] = tag.body;
+// });
+// for (let tag of customTags) {
+//   tags[tag.name] = tag.body;
+// }
 
 // create tagnames list
 var tagNames: Array<string> = [];
 for (let tag of Object.keys(tags)) {
   tagNames.push(tag);
 }
+
+const getTag: any = (tag) => {
+  // @ for user and at for time, validate it
+  let formattedTag = `${tag.toUpperCase()}: ${tags[tag]}`;
+  if (config.user === true) {
+    formattedTag += ` -@${getUser()}`;
+  }
+  if (config.date === true) {
+    formattedTag += ` at ${getDate(config.dateformat)}`;
+  }
+  return formattedTag;
+};
 
 // insert the tags
 var insertTag: any = (editor: vscode.TextEditor, tag: string) => {
@@ -35,16 +63,13 @@ var insertTag: any = (editor: vscode.TextEditor, tag: string) => {
       // delete the selected text
       editBuilder.delete(editor.selection);
       // insert the tag text
-      editBuilder.insert(
-        editor.selection.start,
-        `${tag.toUpperCase()}: ${tags[tag]} -@${getUser()} at ${getDate()}`
-      );
+      editBuilder.insert(editor.selection.start, getTag(tag));
     })
     .then(() => {
       // comment the inserted lines
       vscode.commands.executeCommand("editor.action.commentLine");
-      vscode.commands.executeCommand("editor.action.insertLineAfter");
-      vscode.commands.executeCommand("editor.action.commentLine");
+      // vscode.commands.executeCommand("editor.action.insertLineAfter");
+      // vscode.commands.executeCommand("editor.action.commentLine");
     });
 };
 
